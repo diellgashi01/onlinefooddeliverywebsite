5b4db083eea6ff8bc87f62c3a7203f0b76d083fb
Diell Gashi <dg47881@ubt-uni.net>
2022-08-12 14:27:10 +0200

    Added function to handle uncaught errors

--- backend/server.js
+++ backend/server.js
@@ -2,6 +2,13 @@ const app = require('./app');
 const dotenv = require('dotenv');
 const connectDatabase = require('./config/database');
 
+//Handle Uncaught Exception
+process.on('uncaughtException', err => {
+    console.log(`ERROR: ${err.stack}`);
+    console.log('Shutting down server due to uncaught exception');
+    process.exit(1)
+})
+
 //Setting up config file
 dotenv.config({ path: 'backend/config/config.env'})
 
