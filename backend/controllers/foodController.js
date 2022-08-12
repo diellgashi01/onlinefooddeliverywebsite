83059889d1e427639029d84e40abb855044a7846
Diell Gashi <dg47881@ubt-uni.net>
2022-08-12 11:53:27 +0200

    Created update method for Food products

--- backend/controllers/foodController.js
+++ backend/controllers/foodController.js
@@ -9,7 +9,7 @@ exports.newFood = async (req, res, next) => {
     })
 } 
 
-//Get all Food Products => /api/v1/foods
+//Get all Food Products => /api/admin/v1/foods
 exports.getFoods = async(req, res, next) => {
     const foods = await Food.find();
 
@@ -23,6 +23,7 @@ exports.getFoods = async(req, res, next) => {
 //Get a single Food Product by ID => /api/v1/food/:id
 exports.getSingleFood = async(req, res, next) =>{
     const food = await Food.findById(req.params.id);
+    
     if(!food){
         return res.status(404).json({
             success: false,
@@ -30,6 +31,28 @@ exports.getSingleFood = async(req, res, next) =>{
         })
     }
 
+    res.status(200).json({
+        success: true,
+        food
+    })
+}
+
+//Update Food Product => /api/v1/admin/product/:id
+exports.updateFood = async(req, res, next) =>{
+    let food = await Food.findById(req.params.id);
+    
+    if(!food){
+        return res.status(404).json({
+            success: false,
+            message: 'Food product not found.'
+        })
+    }
+
+    food = await Food.findByIdAndUpdate(req.params.id, req.body, {
+        new: true,
+        runValidators: true,
+        useFindAndModify: false
+    });
     res.status(200).json({
         success: true,
         food
--- backend/routes/food.js
+++ backend/routes/food.js
@@ -1,10 +1,11 @@
 const express = require('express');
 const router = express.Router();
 
-const { getFoods, newFood, getSingleFood } = require('../controllers/foodController');
+const { getFoods, newFood, getSingleFood, updateFood } = require('../controllers/foodController');
 
 router.route('/foods').get(getFoods);
-router.route('/food/new').post(newFood);
 router.route('/food/:id').get(getSingleFood);
+router.route('/admin/food/new').post(newFood);
+router.route('/admin/food/:id').put(updateFood);
 
 module.exports = router;
