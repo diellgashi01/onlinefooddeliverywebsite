9c4e4468820f3c166b834da44d911be6bc32c919
Diell Gashi <dg47881@ubt-uni.net>
2022-08-15 14:55:17 +0200

    small bug fix related to where the authorization is supposed to be

--- backend/routes/food.js
+++ backend/routes/food.js
@@ -4,9 +4,9 @@ const router = express.Router();
 const { getFoods, newFood, getSingleFood, updateFood, deleteFood } = require('../controllers/foodController');
 const { isAuthenticatedUser, authorizeRoles } = require('../middlewares/auth');
 
-router.route('/foods').get(isAuthenticatedUser, authorizeRoles('admin'), getFoods);
+router.route('/foods').get(getFoods);
 router.route('/food/:id').get(getSingleFood);
-router.route('/admin/food/new').post(isAuthenticatedUser, newFood);
-router.route('/admin/food/:id').put(isAuthenticatedUser, updateFood).delete(isAuthenticatedUser, deleteFood);
+router.route('/admin/food/new').post(isAuthenticatedUser, authorizeRoles('admin'), newFood);
+router.route('/admin/food/:id').put(isAuthenticatedUser, authorizeRoles('admin'), updateFood).delete(isAuthenticatedUser, authorizeRoles('admin'), deleteFood);
 
 module.exports = router;
