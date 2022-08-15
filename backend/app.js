a128c812fffe403cfeb8db24d722f4d2616dc30b
Diell Gashi <dg47881@ubt-uni.net>
2022-08-15 08:27:14 +0200

    Registration method for users and password encryption using bcryptjs

--- backend/app.js
+++ backend/app.js
@@ -5,8 +5,10 @@ app.use(express.json());
 
 //Import all routes
 const foods = require('./routes/food');
+const auth = require('./routes/auth');
 
 app.use('/api/v1', foods);
+app.use('/api/v1', auth);
 
 const errorMiddleware = require('./middlewares/errors');
 app.use(errorMiddleware); //Midleware to handle errors
---
+++ backend/controllers/authController.js
@@ -0,0 +1,22 @@
+const User = require('../models/user');
+const ErrorHandler = require('../utils/errorHandler')
+const catchAsyncErrors = require('../middlewares/catchAsyncErrors');
+
+//Register a user => /api/v1/register
+exports.registerUser = catchAsyncErrors( async (req, res, next) => {
+    const { name, email, password } = req.body;
+    const user = await User.create({
+        name,
+        email,
+        password,
+        avatar: {
+            public_id: 'avatars/78-785827_user-profile-avatar-login-account-male-user-icon_tl4l4p',
+            url: 'https://res.cloudinary.com/diellgashi/image/upload/v1660544093/avatars/78-785827_user-profile-avatar-login-account-male-user-icon_tl4l4p.png'
+        }
+    })
+
+    res.status(201).json({
+        success: true,
+        user
+    })
+})
