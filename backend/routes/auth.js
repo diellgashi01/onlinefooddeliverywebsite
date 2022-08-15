836f9de6fdb0a377b833daaea60e4042736e43cf
Diell Gashi <dg47881@ubt-uni.net>
2022-08-15 14:52:12 +0200

    User role authorization/permissions

--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -3,7 +3,7 @@ const router = express.Router();
 
 const { registerUser, loginUser, logout } = require('../controllers/authController')
 
-const { isAuthenticatedUser } = require('../middlewares/auth');
+const { isAuthenticatedUser, authorizeRoles } = require('../middlewares/auth');
 
 router.route('/register').post(registerUser);
 router.route('/login').post(loginUser);
