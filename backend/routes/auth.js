a319dddea8e869bef2641992883ed78e485e5b57
Diell Gashi <dg47881@ubt-uni.net>
2022-08-15 09:55:14 +0200

    Logout method for users

--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -1,11 +1,12 @@
 const express = require('express');
 const router = express.Router();
 
-const { registerUser, loginUser } = require('../controllers/authController')
+const { registerUser, loginUser, logout } = require('../controllers/authController')
 
 const { isAuthenticatedUser } = require('../middlewares/auth');
 
 router.route('/register').post(registerUser);
 router.route('/login').post(loginUser);
+router.route('/logout').get(logout)
 
 module.exports = router;
--- backend/routes/food.js
+++ backend/routes/food.js
@@ -2,10 +2,11 @@ const express = require('express');
 const router = express.Router();
 
 const { getFoods, newFood, getSingleFood, updateFood, deleteFood } = require('../controllers/foodController');
+const { isAuthenticatedUser } = require('../middlewares/auth');
 
 router.route('/foods').get(getFoods);
 router.route('/food/:id').get(getSingleFood);
-router.route('/admin/food/new').post(newFood);
-router.route('/admin/food/:id').put(updateFood).delete(deleteFood);
+router.route('/admin/food/new').post(isAuthenticatedUser, newFood);
+router.route('/admin/food/:id').put(isAuthenticatedUser, updateFood).delete(isAuthenticatedUser, deleteFood);
 
 module.exports = router;
