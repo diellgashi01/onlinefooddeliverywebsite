33b9e4a9b6b2c634eae3dea1d8f6f4dabc786eb0
Diell Gashi <dg47881@ubt-uni.net>
2022-08-15 08:51:24 +0200

    Login function for users

--- backend/controllers/authController.js
+++ backend/controllers/authController.js
@@ -22,3 +22,32 @@ exports.registerUser = catchAsyncErrors( async (req, res, next) => {
         token
     })
 })
+
+//Login user method => /api/v1/login
+exports.loginUser = catchAsyncErrors( async(req, res, next) => {
+    const { email, password } = req.body;
+    
+    //Checks if email and password are entered 
+    if(!email || !password){
+        return next(new ErrorHandler('Please enter email and password!', 400))
+    }
+
+    //Finding user in database
+    const user = await User.findOne({ email }).select('+password')
+    if(!user){
+        return next(new ErrorHandler('Invalid Email or Password', 401));
+    }
+
+    //Checks if password is correct
+    const isPasswordMatched = await user.comparePassword(password);
+    if(!isPasswordMatched){
+        return next(new ErrorHandler('Invalid Email or Password', 401));
+    }
+
+    const token = user.getJwtToken();
+
+    res.status(200).json({
+        success: true,
+        token
+    })
+})
