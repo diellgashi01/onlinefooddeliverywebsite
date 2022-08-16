cc4e08f97e12416921aad9f897c03c7780f50421
Diell Gashi <dg47881@ubt-uni.net>
2022-08-16 10:53:26 +0200

    Handled JWT invalid/expired errors and also Duplicate Key Error

--- backend/middlewares/errors.js
+++ backend/middlewares/errors.js
@@ -29,6 +29,24 @@ module.exports = (err, req, res, next) => {
             error = new ErrorHandler(message, 400)
         }
 
+        //Handling Mongoose Duplicate Key Error
+        if(err.code === 11000){
+            const message = `Duplicate ${Object.keys(err.keyValue)} entered.`
+            error = new ErrorHandler(message, 400)
+        }
+
+        //Handling Wrong JWT Error
+        if(err.name === 'JsonWebTokenError'){
+            const message = 'JWT is invalid.'
+            error = new ErrorHandler(message, 400)
+        }
+
+        //Handling Expired JWT Error
+        if(err.name === 'TokenExpiredError'){
+            const message = 'JWT is expired.'
+            error = new ErrorHandler(message, 400)
+        }
+
         res.status(error.statusCode).json({
             success: false,
             message: error.message || 'Internal Server Error'
