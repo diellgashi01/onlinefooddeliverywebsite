260e8f0f5577130a49dc01160a9c28f831029a56
Diell Gashi <dg47881@ubt-uni.net>
2022-08-17 14:34:48 +0200

    Get Order by ID and User's Orders methods

--- backend/controllers/orderController.js
+++ backend/controllers/orderController.js
@@ -19,3 +19,27 @@ exports.newOrder = catchAsyncErrors(async (req, res, next) => {
         order
      })
 })
+
+//Get single order => /api/v1/order/:id
+exports.getSingleOrder = catchAsyncErrors(async (req, res, next) =>{
+    const order = await Order.findById(req.params.id).populate('user', 'name email')
+
+    if(!order) {
+        return next(new ErrorHandler('Order ID does not exist', 404))
+    }
+
+    res.status(200).json({
+        success: true,
+        order
+    })
+})
+
+//Get logged in user's orders => /api/v1/orders/me
+exports.myOrders = catchAsyncErrors(async (req, res, next) =>{
+    const orders = await Order.find({ user: req.user.id })
+
+    res.status(200).json({
+        success: true,
+        orders
+    })
+})
