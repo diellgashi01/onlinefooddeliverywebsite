cf9e88916870b361192528ce26c46c2e906e6576
Altin Salihu <109679171+AltinQx@users.noreply.github.com>
2022-08-20 15:28:52 +0200

    deleteOrder

--- backend/controllers/orderController.js
+++ backend/controllers/orderController.js
@@ -89,3 +89,18 @@ async function updateStock(id, quantity) {
 
     await food.save({ validateBeforeSave: false })
 }
+
+//Delete order => /api/v1/admin/order/:id
+exports.deleteOrder = catchAsyncErrors(async (req, res, next) =>{
+    const order = await Order.findById(req.params.id)
+
+    if(!order) {
+        return next(new ErrorHandler('Order ID does not exist', 404))
+    }
+
+    await order.remove()
+
+    res.status(200).json({
+        success: true,
+    })
+})
