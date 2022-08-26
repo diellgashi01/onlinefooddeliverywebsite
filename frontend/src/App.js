35b95a804edef6c3ac5eb4c8f891f9915fd9dde4
Diell Gashi <dg47881@ubt-uni.net>
2022-08-26 14:17:30 +0200

    Random header added

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,8 +1,10 @@
 import './App.css';
+import Header from './components/layout/Header'
 
 function App() {
   return (
     <div className="App">
+      <Header />
       <h1>Food Delivery</h1>
     </div>
   );
